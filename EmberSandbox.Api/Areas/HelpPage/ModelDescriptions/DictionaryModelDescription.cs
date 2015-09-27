399e3e0d3472cab569b6707d1c5a3b98fb45c1a5
Michael Schwartz <michael.schwartz76@gmail.com>
2015-09-27 09:42:08 -0600

    Added Web and Api projects to solution

---
+++ EmberSandbox.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace EmberSandbox.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ EmberSandbox.Api/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EmberSandbox.Api.Startup))]
+
+namespace EmberSandbox.Api
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
